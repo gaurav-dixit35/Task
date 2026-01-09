78426a18c907b66b8ddf64200927a4b95aa94dff
Gaurav Dixit <gd36990@gmail.com>
2026-01-09 11:36:53 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,9 +1,10 @@
-// firebase.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+import {
+  getAuth,
+  GoogleAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBOEs1Ibj8v636R8IgzM5D6xqJkUzWjgko",
   authDomain: "task-759da.firebaseapp.com",
@@ -11,10 +12,9 @@ const firebaseConfig = {
   storageBucket: "task-759da.firebasestorage.app",
   messagingSenderId: "297266929297",
   appId: "1:297266929297:web:637925e90365c710a8e930",
-  measurementId: "G-JTPDDSTWDP"
+  measurementId: "G-JTPDDSTWDP",
 };
 
-// Init
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
